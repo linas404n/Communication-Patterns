0a16ed36cf4c2d3e9cd851ddc993b6a63a80e41f
Linas <linasn404@gmail.com>
2024-08-15 10:10:17 +0200

    Create project

---
+++ CommunicationPatterns/CommunicationPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunicationPatternsApp.swift
+//  CommunicationPatterns
+//
+//  Created by Linas on 15/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CommunicationPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommunicationPatterns/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CommunicationPatterns
+//
+//  Created by Linas on 15/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
